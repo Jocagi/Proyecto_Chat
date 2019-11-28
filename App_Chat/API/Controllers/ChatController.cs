b6d5da4b7ec734b444b8be563a46fb844b5bfd1f
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-11-28 13:28:06 -0600

    Chat Database Model

---
+++ App_Chat/API/Controllers/ChatController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+
+        private readonly ChatService _ChatService;
+
+        public ChatController(ChatService ChatService)
+        {
+            _ChatService = ChatService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Chat>> Get() =>
+            _ChatService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetChat")]
+        public ActionResult<Chat> Get(string id)
+        {
+            var Chat = _ChatService.Get(id);
+
+            if (Chat == null)
+            {
+                return NotFound();
+            }
+
+            return Chat;
+        }
+
+        [HttpPost]
+        public ActionResult<Chat> Create(Chat Chat)
+        {
+            _ChatService.Create(Chat);
+
+            return CreatedAtRoute("GetChat", new { id = Chat.Id.ToString() }, Chat);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Chat ChatIn)
+        {
+            var Chat = _ChatService.Get(id);
+
+            if (Chat == null)
+            {
+                return NotFound();
+            }
+
+            _ChatService.Update(id, ChatIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var Chat = _ChatService.Get(id);
+
+            if (Chat == null)
+            {
+                return NotFound();
+            }
+
+            _ChatService.Remove(Chat.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ App_Chat/API/Models/Chat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace API.Models
+{
+    public class Chat
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id;
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string ChatID;
+        public List<Message> mensajes;
+    }
+}
