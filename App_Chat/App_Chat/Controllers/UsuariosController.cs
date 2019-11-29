641aaac21d45e144693f7670765eaf27d9abd02f
genecc15 <genecc15@gmail.com>
2019-11-29 08:41:25 -0600

    IngresadoUsuarios

---
+++ App_Chat/App_Chat/Controllers/UsuariosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using App_Chat.Models;
+
+namespace App_Chat.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            IEnumerable<UsuariosController> Usuario = null;
+            using (var usuario = new HttpClient())
+            {
+                usuario.BaseAddress = new Uri("http://localhost:44316/");
+                var responseTask = usuario.GetAsync("usuario");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<IList<UsuariosController>>();
+                    readJob.Wait();
+                    Usuario = readJob.Result;
+                }
+                else
+                {
+                    Usuario = Enumerable.Empty<UsuariosController>();
+                    ModelState.AddModelError(string.Empty, "No se puede ingresar el usuario");
+                }
+            }
+            return View(Usuario);
+        }
+    }
+}
---
+++ App_Chat/App_Chat/Models/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App_Chat.Models
+{
+    public class Usuarios
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string User { get; set; }
+    }
+}
