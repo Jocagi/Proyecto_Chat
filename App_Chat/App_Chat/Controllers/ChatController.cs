17f3fbe3c23e5cf3d4ea2a21339eb22c2186e42d
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-11-28 22:54:20 -0600

    Definir chat id en vista

--- App_Chat/App_Chat/Controllers/ChatController.cs
+++ App_Chat/App_Chat/Controllers/ChatController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using App_Chat.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace App_Chat.Controllers
 {
@@ -11,7 +13,31 @@ namespace App_Chat.Controllers
         // GET: Chat
         public ActionResult Index()
         {
-            return View();
+            return View(obtenerContactos());
         }
+
+        private ListaDeContactos obtenerContactos()
+        {
+            List<Contacto> contactos = new List<Contacto>() {new Contacto("Jose", "~/Images/Gaticornio.png"), new Contacto("Genesis", "~/Images/pato.jpg") };
+            ListaDeContactos nuevo = new ListaDeContactos(contactos);
+            nuevo.usuario = "Bob";
+            nuevo.foto = "~/Images/pato.jpg";
+
+            return nuevo;
+        }
+
+        [HttpGet]
+        public string Mensajes(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                return $"<li class=\"sent\"><p>{id}</p> </li> <li class=\"replies\"><p>Mucho gusto</p></li>";
+            }
+            else
+            {
+                return "";
+            }
+        }
+        
     }
 }
---
+++ App_Chat/App_Chat/Models/Contacto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App_Chat.Models
+{
+    public class Contacto
+    {
+        public string username { get; set; }
+        public string imagen { get; set; }
+
+        public Contacto()
+        { }
+
+        public Contacto(string usr, string img)
+        {
+            this.username = usr;
+            this.imagen = img;
+        }
+    }
+}
